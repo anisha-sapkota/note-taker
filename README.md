4e188b6801cbce45d497041835fbc56c6b2c3808
Anisha Sapkota <anisha.sapkota79@gmail.com>
2022-10-16 20:22:46 +1100

    added comments and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # note-taker
-Note taker to save and write notes.
+
+Note taker application using JavaScript.
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+```sh
+npm install
+```
+
+## Usage
+
+Run the following command to start the application:
+
+```sh
+npm start
+```
+
+Use the UI to list, add and delete notes.The live application can be accessed [here](https://anisha-note-taker.herokuapp.com).
+
+## Technologies used
+
+- uuid
+- Express
+
+## License
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Helpful resources
+
+- <https://github.com/uuidjs/uuid#readme>
+- <https://modernweb.com/the-basics-of-express-routes/>
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-// import express library
+// import required libraries
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 const path = require("path");
 const fs = require("fs");
+
+// import notes
 const notes = require("./db/db.json");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// function for writing given data to 'db.json'
 const writeToFile = (data) => {
   fs.writeFile(path.join(__dirname, "/db/db.json"), data, (err) =>
     err ? console.log(err) : console.log(`Successfully updated database!`)
@@ -16,18 +19,21 @@ const writeToFile = (data) => {
 
 // Middleware for parsing application/json
 app.use(express.json());
+
 // serve static content from public folder
 app.use(express.static("public"));
 
-// GET Route for notes
+// GET Route for notes.html
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
+// GET route for notes api
 app.get("/api/notes", (req, res) => {
   res.json(notes);
 });
 
+// DELETE route for notes api
 app.delete("/api/notes/:id", (req, res) => {
   const id = req.params.id;
   // find index of note with given id
@@ -42,9 +48,11 @@ app.delete("/api/notes/:id", (req, res) => {
   }
 });
 
+// POST route for notes api
 app.post("/api/notes", (req, res) => {
   if (req.body) {
     if (req.body.title && req.body.text) {
+      // generate random id using uuidv4
       const note = { id: uuidv4(), ...req.body };
       notes.unshift(note);
       writeToFile(JSON.stringify(notes, null, 2));
@@ -57,7 +65,7 @@ app.post("/api/notes", (req, res) => {
   }
 });
 
-// catch-all
+// catch-all - any other route, return index.html
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
